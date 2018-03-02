a6ab8473c6d6f5fbd6b4e1e38f9fd52e66a6a3f4
Luka Tadic <luka.tadic@vingd.com>
2018-03-03 00:21:12 +0100

    made cmus control over i3block with rofi as song selector

---
+++ .i3/scripts/cmus-player.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+export XDG_RUNTIME_DIR='/tmp/1000-runtime-dir'
+buttons() {
+  case "$BLOCK_BUTTON" in
+    1)
+      # left click, choose song
+      song="$(xargs -a ~/.config/cmus/lib.pl -d '\n' basename -s .mp3 | rofi -dmenu -i -no-fullscreen  -p "" -m -3 -no-custom -theme-str '#window {anchor:southeast; location: northwest;}' -width 40 -no-custom | grep -f -  ~/.config/cmus/lib.pl)"
+      if [ ! -z "$song" ]; then
+        cmus-remote -C "player-play $song"
+        pkill -RTMIN+11 i3blocks
+      fi
+      ;;
+    2)
+      # middle click, pause
+      cmus-remote --pause
+      ;;
+    3)
+      # right click, prev song
+      cmus-remote --prev
+      ;;
+    4)
+      # scroll up, next song
+      cmus-remote --next
+      ;;
+    5)
+      # scroll down, previous song
+      cmus-remote --prev
+      ;;
+    esac
+}
+buttons
+data="$(cmus-remote -Q | sed -n "/^status/,/^file/s/status \(.*\)/\1/p; s/file \(.*\)/\1/p" | tr '\n' ' ')"
+read status file <<< "$data"
+file="$(basename "$file")"
+file="${file%.mp3}"
+file="${file//&}"
+
+case "$status" in 
+  "playing")
+    status="♫"
+    ;;
+  "stopped")
+    status="⏸"
+    ;;
+  "paused")
+    status="■"
+    ;;
+esac
+
+printf "%s %4.30s\n" "$status" "$file"
